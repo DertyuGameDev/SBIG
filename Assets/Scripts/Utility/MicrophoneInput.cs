569c0d50fdb0e931c225cad88e6bb82414001971
Dertyu <110996909+DertyuGameDev@users.noreply.github.com>
2024-06-27 14:01:53 +0200

    pr #1 Microphone Input + Settings

---
+++ Assets/Scripts/Utility/MicrophoneInput.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicrophoneInput : MonoBehaviour
+{
+    public int sampleWindow = 64;
+    public string nameMicro;
+    public float maxSenc, minSens;
+    public float sensitivityMicrophone;
+    public float thresold;
+    AudioClip clip;
+    public Slider slider, sensSlider;
+    public TMP_Dropdown drop;
+
+    public void MicrophoneChoose()
+    {
+        nameMicro = Microphone.devices[drop.value];
+        clip = Microphone.Start(nameMicro, true, 20, AudioSettings.outputSampleRate);
+    }
+
+    public float GetLouder(int clipPos, AudioClip clip)
+    {
+        int startPos = clipPos - sampleWindow;
+
+        if (startPos < 0) { return 0; }
+
+        float[] waveData = new float[sampleWindow];
+        clip.GetData(waveData, startPos);
+
+        float totalLoudness = 0;
+
+        foreach (var wave in waveData)
+        {
+            totalLoudness += Mathf.Abs(wave);
+        }
+
+        return totalLoudness / sampleWindow;
+    }
+    private void Start()
+    {
+        List<TMP_Dropdown.OptionData> listOptions = new List<TMP_Dropdown.OptionData>();
+
+        for (int i = 0; i < Microphone.devices.Length; i++)
+        {
+            TMP_Dropdown.OptionData g = new TMP_Dropdown.OptionData();
+            g.text = Microphone.devices[i];
+            listOptions.Add(g);
+        }
+        drop.options = listOptions;
+        sensSlider.minValue = minSens; 
+        sensSlider.maxValue = maxSenc; 
+        sensSlider.value = PlayerPrefs.GetFloat("Sensitivity", 1);
+        MicrophoneChoose();
+    }
+    private void Update()
+    {
+        PlayerPrefs.SetFloat("Sensitivity", sensSlider.value);
+        sensitivityMicrophone = sensSlider.value;
+        float loud = GetLouder(Microphone.GetPosition(nameMicro), clip);
+
+        if (loud < thresold) { loud = 0; }
+        print(loud);
+        slider.value = loud * PlayerPrefs.GetFloat("Sensitivity");
+    }
+}
